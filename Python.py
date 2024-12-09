0bf1fa4b4c313275d6b54ad4d680f14100ed709e
Mohammad Davodi <mohammaddavodi96@gmail.com>
2024-12-10 00:03:15 +0330

    Cars

--- Python.py
+++ Python.py
@@ -1,34 +1,24 @@
-def get_number(msg: str, min_number: int = 0, max_number: int = 300 ) -> int:
-    number = ""
-    while True:
-        number = input(msg)
-        if number.isdigit():
-            number = int(number)
-            if number >= min_number and number <= max_number:
-                break
-    return int(number)    
+# def check(cars: list):
+#     cars.append("BMW")
+#     print(cars)
+# vehicles = ["audi", "volvo"]
+# check(vehicles[:])  
+# print(vehicles)  
 
-def calculate_bmi(height: int, weight: int) -> float:
-    bmi = 10_000 * (weight / height / weight)
-    return bmi
+ 
+def is_car_available(year: int ,*cars):
+    cars_available = {2024: ["BMW", "Audi", "Tesla"],
+                      2023: ["Volvo"]}
 
-def get_bmi_range(bmi: float) -> str:
-        if bmi < 18.5:
-            return "Underweight"
-        if bmi >= 18.5 and bmi < 24.9:
-            return "Healthy"
-        if bmi >= 25 and bmi < 29.9:
-            return "Overweight"
-        if bmi >= 30:
-            return "obese"
-        return "Unknow"               
-
-height = get_number(msg="Height?", min_number=0 , max_number=200 )
-weight = get_number(msg="Weight?")
-
-bmi = calculate_bmi(height=height , weight=weight)
-bmi_rage = get_bmi_range(bmi)
-bmi = "{:.2f}".format(bmi)
-
-print(bmi)
-print(bmi_rage)
+    models = cars_available.get(year)
+    if not models:
+        print(f"'{year}' Not faund")
+        return
+    models = [model.strip().lower() for model in models]    
+    print(models, cars)
+    for car in cars:
+        if car.strip().lower() in models:
+            print(f"{car.title()} is available")    
+        else:
+            print(f"Unfortunatley '{car.title()}' is not available")
+is_car_available(2024,"audi","bMW")    
